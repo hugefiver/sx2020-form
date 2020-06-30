910595cb37df9b619479f8cee7c3d41656429fc1
hugefiver <i@iruri.moe>
2020-06-30 23:04:40 +0800

    first page

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+
+import { Index } from "./pages";
+
+export const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Index />
+                </Route>
+            </Switch>
+        </Router>
+
+    )
+}
+
+render(<App/>, document.getElementById('app-root'));
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Helmet} from 'react-helmet';
+
+export function Index() {
+    return (
+        <>
+            <Helmet>
+                <title>调查问卷 - 首页</title>
+            </Helmet>
+            <div>
+                <p>Hello</p>
+            </div>
+        </>
+    )
+}
