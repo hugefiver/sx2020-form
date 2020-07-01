adea50bba162bb183dfa253a23f1c371fd580651
hugefiver <i@iruri.moe>
2020-07-01 23:36:10 +0800

    f*ck the f^cking fluentui and the f$cking microsoft document

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
-import {Customizer} from '@fluentui/react';
-import {FluentCustomizations} from '@uifabric/fluent-theme/lib-commonjs';
 
 
 import * as paths from './utils/paths';
 
-import {Index} from './pages';
-import Register from "./pages/register";
 
 export const App = () => {
     return (
-        <Customizer settings={FluentCustomizations.settings}
-                    scopedSettings={FluentCustomizations.scopedSettings}>
             <Router>
                 <Switch>
                     <Route exact path="/">
-                        <Index />
+
                     </Route>
                     <Route exact path={paths.register}>
-                        <Register />
+
                     </Route>
                 </Switch>
             </Router>
-        </Customizer>
     )
 }
 
